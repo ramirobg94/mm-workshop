5d8a9b67a5b3d37339c50c21556ece9a5e938644
ramirobg94 <ramirobg94@gmail.com>
2018-04-07 19:13:27 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      currentView: 'home'
+    }
+
+    this.renderView = this.renderView.bind(this)
+    this.changeView = this.changeView.bind(this)
+  }
+
+  changeView(view){
+    this.setState({currentView: view})
+  }
+
+  renderView(){
+    switch(this.state.currentView){
+      case 'home':
+        return <Home changeView={()=>this.changeView('funnel')}/>
+        break
+      case 'funnel':
+        return <Funnel changeView={()=>this.changeView('thankYou')}/>
+        break
+      case 'thankYou':
+        return <ThankYou changeView={()=>this.changeView('home')}/>
+        break
+      default:
+        return <Home />
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderView()}
       </div>
     );
   }
 }
+
+const Home = ({changeView}) => <div><p>home</p><div onClick={()=>changeView()}>To funnel</div></div>
+const Funnel = ({changeView}) => <div><p>Funnel</p><div onClick={()=>changeView()}>To ThankYou</div></div>
+const ThankYou = ({changeView}) => <div><p>ThankYou</p><div onClick={()=>changeView()}>To Home</div></div>
 
 export default App;
